b1eb75fd2570d05a270b52ae8f88e7f3c38eed9a
Lucas <viper20@poczta.fm>
2017-09-28 17:48:43 +0200

    Express Auth Google

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': '553059959691-mg3q9cr8kl0d53hvhk7amvccit32omcb.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': '2UPADoFtEowHYm8aXFf2MD-L',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
