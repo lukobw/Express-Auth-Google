93337cb7e467ef3bf1d9e02ede851291ad2bf1fd
Lucas <viper20@poczta.fm>
2017-10-01 12:07:00 +0200

    fix

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    'GOOGLE_CLIENT_ID': '553059959691-mg3q9cr8kl0d53hvhk7amvccit32omcb.apps.googleusercontent.com',
-    'GOOGLE_CLIENT_SECRET': '2UPADoFtEowHYm8aXFf2MD-L',
+    'GOOGLE_CLIENT_ID': '699394405231-vq6tdprsgjeinaqb6cpdnq07h4j0621s.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': 'HtToB5e19G6xAGI4ptREMhD5',
     'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
 };
